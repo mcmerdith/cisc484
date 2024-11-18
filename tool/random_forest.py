1b6b5ac05cef1f3fa777c178ffaface7bc01efac
Matthew Meredith <merematt@udel.edu>
2024-11-17 19:21:57 -0500

    it at least doesn't crash. it's still bad tho

--- tool/random_forest.py
+++ tool/random_forest.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 import datetime
 from functools import reduce
+from typing import Literal
 import numpy as np
 from sklearn.metrics import accuracy_score
 from numpy.random import default_rng
@@ -8,18 +9,122 @@ from numpy.random import default_rng
 DEBUG = True
 VERBOSE = False
 
+UntrainedValueMode = Literal["majority", "nearest"]
+"""How the model should handle unknown values
 
-def DEBUG_PRINT(*args, verbose: bool = False,  **kwargs):
-    if VERBOSE:
-        print("[VERBOSE]", *args, **kwargs)
-    else:
-        print("[DEBUG]", *args, **kwargs)
+Options
+-------
+    `majority`
+        return the most common label
+
+    `nearest`
+        return the label closest to the unknown value"""
+
+
+def DEBUG_PRINT(*args, **kwargs):
+    print("[DEBUG]", *args, **kwargs)
+
+
+def VERBOSE_PRINT(*args, **kwargs):
+    DEBUG_PRINT("[VERBOSE]", *args, **kwargs)
 
 
 def DEBUG_SPACER(shape) -> np.ndarray:
     spacer = np.zeros(shape, dtype=np.str_)
     spacer.fill("=")
     return spacer
+
+
+@dataclass
+class BaseNode:
+    feature_idx: int
+    """The index of the feature this node predicts"""
+
+    def _get_branch(self, X: float, untrained_value_mode: UntrainedValueMode = None) -> "BaseNode":
+        """Get the value of the branch for a sample.
+
+        Must be implemented by subclasses if `predict` is not overridden.
+
+        Parameters
+        ----------
+            `X` : int
+                The value of the feature to predict the label for
+
+            `untrained_value_mode` : UntrainedValueMode
+
+        Returns
+        -------
+            A `BaseNode` or None if there is no suitable prediction"""
+        raise NotImplementedError()
+
+    def predict(self, sample: np.ndarray, untrained_value_mode: UntrainedValueMode = None) -> int:
+        """Predict the label for a dataset
+
+        Parameters
+        ----------
+            `sample` : np.ndarray<M>
+                A sample of features
+
+            `untrained_value_mode` : UntrainedValueMode
+        """
+        assert len(sample.shape) == 1, "sample must be a 1D array"
+
+        predictor = self._get_branch(
+            sample[self.feature_idx], untrained_value_mode)
+
+        if isinstance(predictor, BaseNode):
+            return predictor.predict(sample, untrained_value_mode)
+        else:
+            print("[PANIC] Failed to predict value of feature",
+                  self.feature_idx, sample[self.feature_idx])
+
+        return predictor
+
+
+@dataclass
+class LeafNode(BaseNode):
+    """A leaf node in a decision tree
+
+    The value of `feature_idx` is ignored"""
+    label: float
+
+    def predict(self, _: np.ndarray, __: UntrainedValueMode = None) -> float:
+        return self.label
+
+
+@dataclass
+class StandardNode(BaseNode):
+    branches: dict[float, BaseNode]
+    majority_label: float
+
+    def _get_branch(self, X: float, untrained_value_mode: UntrainedValueMode = None) -> BaseNode:
+        branch = self.branches.get(X)
+        if branch is None and untrained_value_mode is not None:
+            if untrained_value_mode == "majority":
+                if DEBUG:
+                    DEBUG_PRINT("No branch found, returning majority label",
+                                self.feature_idx, "[", X, "] =", self.majority_label)
+                return self.majority_label
+            elif untrained_value_mode == "nearest":
+                keys = np.array(list(self.branches.keys()))
+                nearest = keys[np.argmin(np.abs(keys - X))]
+                if DEBUG:
+                    DEBUG_PRINT("No branch found, returning nearest label",
+                                self.feature_idx, "[", X, "] ->", nearest)
+                return self.branches[nearest]
+        return branch
+
+
+@dataclass
+class RealValueNode(BaseNode):
+    threshold: float
+    left: BaseNode
+    right: BaseNode
+
+    def _get_branch(self, X: float, _: UntrainedValueMode = None) -> BaseNode:
+        if X < self.threshold:
+            return self.left
+        return self.right
 
 
 def _n_log_n(n: float) -> float:
@@ -81,6 +186,11 @@ def _information_gain(x: np.ndarray, y: np.ndarray) -> list[float]:
         `y` : np.ndarray<1>
             A 1D array of the label values
 
+    Returns
+    -------
+        `information_gain` : list[float]<M>
+            The information gain of each feature
+
     Restrictions
     ----------
         `N` and `y.size` must be equal
@@ -96,13 +206,15 @@ def _information_gain(x: np.ndarray, y: np.ndarray) -> list[float]:
         for i in range(x.shape[1])
     ]
     if VERBOSE:
-        DEBUG_PRINT(f"Information gain of\n{
-            np.c_[x, DEBUG_SPACER(y.shape), y]}\n{information_gain}\n", verbose=True)
+        VERBOSE_PRINT(
+            f"Information gain of\n{np.c_[x, DEBUG_SPACER(y.shape), y]}\n{
+                information_gain}\n"
+        )
     return information_gain
 
 
-def _partition(x: np.ndarray, y: np.ndarray) -> dict[int, tuple[np.ndarray, np.ndarray]]:
-    """Partition `x` according to the feature of `x` with the highest information gain
+def _partition(x: np.ndarray, y: np.ndarray, feature: int) -> dict[float, tuple[np.ndarray, np.ndarray]]:
+    """Partition `x` according to `feature`
 
     Parameters
     ----------
@@ -112,125 +224,158 @@ def _partition(x: np.ndarray, y: np.ndarray) -> dict[int, tuple[np.ndarray, np.n
         `y` : np.ndarray<N>
             A 1D array of the label values
 
+        `feature` : int
+            The index of the feature to partition on
+
     Returns
     -------
-        `partitions` : dict[int, tuple[np.ndarray<N,M>, np.ndarray<N>]]
+        `partitions` : dict[float, tuple[np.ndarray<N,M>, np.ndarray<N>]]
             A dict of `S` partitions of `x` according to the feature of `x`
             with the highest information gain and the corresponding label values
     """
 
-    # Select the highest information gain feature
-    selected_feature = np.argmax(_information_gain(x, y))
-
     if VERBOSE:
-        DEBUG_PRINT("Creating partition on feature",
-                    selected_feature, "\n", verbose=True)
+        VERBOSE_PRINT("Creating partition on feature", feature, "\n")
 
     # Get the values of the selected feature
-    feature = x[:, selected_feature]
+    feature_set = x[:, feature]
+    if DEBUG:
+        DEBUG_PRINT("Feature value shape", feature_set.shape)
     if VERBOSE:
-        DEBUG_PRINT(f"Feature values\n{
-            np.c_[feature, DEBUG_SPACER(y.shape), y]}\n", verbose=True)
+        VERBOSE_PRINT(
+            f"Feature values\n{np.c_[feature_set, DEBUG_SPACER(y.shape), y]}\n"
+        )
 
-    # Copy the array without the selected feature
-    new_data = np.delete(x, selected_feature, axis=1)
     # Partition the new array according to the value of the feature
     partitions = {
         feature_value: (
-            new_data[x[:, selected_feature] == feature_value, :],
-            y[x[:, selected_feature] == feature_value]
-        ) for feature_value in np.unique(feature)
+            x[feature_set == feature_value, :],
+            y[feature_set == feature_value]
+        ) for feature_value in np.unique(feature_set)
     }
 
     if VERBOSE:
-        DEBUG_PRINT(*[f"\nPartition created for {feature_value}\n{np.c_[data, DEBUG_SPACER(labels.shape), labels]}\n"
-                      for feature_value, (data, labels) in partitions.items()], verbose=True)
+        VERBOSE_PRINT(*[f"\nPartition created for {feature_value}\n{np.c_[data, DEBUG_SPACER(labels.shape), labels]}\n"
+                      for feature_value, (data, labels) in partitions.items()])
     return partitions
 
 
-class BaseNode:
-    def _get_branch(self, X: int) -> "BaseNode":
-        raise NotImplementedError()
-
-    def predict(self, X: int) -> bool:
-        predictor = self._get_branch(X)
-        if isinstance(predictor, BaseNode):
-            return predictor.predict(X)
-        else:
-            print("[PANIC] Failed to predict value for", X)
-        return predictor
-
-
-@dataclass
-class LeafNode(BaseNode):
-    value: bool
-
-    def predict(self, _: int) -> bool:
-        return self.value
-
-
-@dataclass
-class StandardNode(BaseNode):
-    branches: dict[int, BaseNode]
-
-    def _get_branch(self, X: int) -> BaseNode:
-        return self.branches.get(X)
-
-
-@dataclass
-class RealValueNode(BaseNode):
-    threshold: int
-    left: BaseNode
-    right: BaseNode
-
-    def _get_branch(self, X: int) -> BaseNode:
-        if X < self.threshold:
-            return self.left
-        return self.right
-
-
 def _most_common(x: np.ndarray) -> int:
+    """Return the most common value in `x`
+
+    Parameters
+    ----------
+        `x` : np.ndarray
+            An array of values
+    """
     possible = np.unique(x)
     frequency = [len(x[x == value]) for value in possible]
     return possible[np.argmax(frequency)]
 
 
-def _create_node(x: np.ndarray, y: np.ndarray) -> BaseNode:
+def _create_node(x: np.ndarray, y: np.ndarray, *, _include: np.ndarray = None) -> BaseNode:
+    """Create a decision tree node from `x` and `y`
+
+    Parameters
+    ----------
+        `x` : np.ndarray<N,M>
+            A 2D array containing `N` samples of `M` features
+
+        `y` : np.ndarray<1>
+            A 1D array of the label values
+
+    Returns
+    -------
+        `node` : BaseNode
+            A decision tree node
+
+    Restrictions
+    ------------
+        `N` and `y.size` must be equal
+    """
+    assert x.shape[0] == y.size, "x and y must have the same size"
+
+    ### Base Cases ###
+
     # If there is only one label, return a leaf node
     unique_labels = np.unique(y)
     if unique_labels.size == 1:
         if VERBOSE:
-            DEBUG_PRINT("only one label", unique_labels, verbose=True)
-        return LeafNode(value=unique_labels[0])
+            VERBOSE_PRINT("only one label", unique_labels)
+        return LeafNode(feature_idx=None, label=unique_labels[0])
 
     # If there is only one set of inputs, return a leaf node
     unique_values = np.unique(x, axis=0)
     if len(unique_values) == 1:
         if VERBOSE:
-            DEBUG_PRINT("only one value", unique_values, verbose=True)
-        return LeafNode(value=_most_common(y))
+            VERBOSE_PRINT("only one value", unique_values)
+        return LeafNode(feature_idx=None, label=_most_common(y))
+
+    ### Recursive Behavior ###
 
     # Otherwise partition the data
-    partitions = _partition(x, y)
-    nodes: dict[int, BaseNode] = {}
+    # Select the highest information gain feature
 
-    if VERBOSE:
-        for feature, (data, labels) in partitions.items():
-            DEBUG_PRINT(
-                f"Partition {feature}\n{np.c_[data, DEBUG_SPACER(labels.shape), labels]}\n", verbose=True)
+    if _include is None:
+        if DEBUG:
+            DEBUG_PRINT("Initializing to include all features")
+        # initial state is to include all features
+        _include = np.array([True] * x.shape[1])
+        information_gain = _information_gain(x, y)
+        if DEBUG:
+            DEBUG_PRINT("Information gain", information_gain)
+        feature_idx = np.argmax(information_gain)
+    else:
+        if DEBUG:
+            DEBUG_PRINT("Using features", _include)
+        # get the feature index with the highest information gain
+        feature_idx = np.argmax(_information_gain(x[:, _include], y))
+        # convert the feature index to the original index
+        feature_idx = _include.nonzero()[0][feature_idx]
+
+    # exclude the feature from the next iteration
+    _include[feature_idx] = False
+
+    if DEBUG:
+        DEBUG_PRINT("Branching on feature", feature_idx)
+
+    partitions = _partition(x, y, feature_idx)
+    nodes: dict[float, BaseNode] = {}
+
+    if DEBUG:
+        DEBUG_PRINT("Creating", len(partitions), "partitions")
+        for feature_value, (p_data, p_labels) in partitions.items():
+            DEBUG_PRINT(f"Partition {feature_value} contains {
+                        p_data.shape[0]} samples and {p_labels.shape[0]} labels")
+            if VERBOSE:
+                VERBOSE_PRINT(
+                    f"Partition {feature_value}\n{
+                        np.c_[p_data, DEBUG_SPACER(p_labels.shape), p_labels]}\n"
+                )
 
     # Create the nodes for each partition
-    for feature, (data, labels) in partitions.items():
-        nodes[feature] = _create_node(data, labels)
+    for feature_value, (p_data, p_labels) in partitions.items():
+        nodes[feature_value] = _create_node(
+            p_data, p_labels, _include=_include)
 
-    return StandardNode(branches=nodes)
+    return StandardNode(feature_idx=feature_idx, branches=nodes, majority_label=_most_common(y))
 
 
 class MMDecisionTree:
-    def __init__(self):
+    def __init__(self, untrained_value_mode: UntrainedValueMode = "nearest"):
+        """A decision tree classifier
+
+        Parameters
+        ----------
+            `untrained_value_mode` : "majority" | "nearest"
+                How the model should handle unknown values
+                "majority" will return the most common label
+                "nearest" will return the label closest to the unknown value
+        """
         self._tree: BaseNode = None
         self._train_data: np.ndarray = None
         self._train_labels: np.ndarray = None
+        self._untrained_value_mode = untrained_value_mode
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
         self._train_data = X
@@ -241,7 +386,10 @@ class MMDecisionTree:
         return accuracy_score(y, self.predict(X))
 
     def predict(self, X: np.ndarray) -> np.ndarray:
-        return np.zeros(X.shape[0])
+        return np.array([
+            self.tree.predict(X[i, :], self._untrained_value_mode)
+            for i in range(X.shape[0])
+        ])
 
 
 class MMRandomForest:
@@ -259,7 +407,9 @@ class MMRandomForest:
             DEBUG_PRINT("Training started at", start)
         self._train_data = X
         self._train_labels = y
-        for _ in range(self._n_trees):
+        for i in range(self._n_trees):
+            if DEBUG:
+                DEBUG_PRINT("Training tree", i)
             random = default_rng()
             samples = random.choice(
                 self._train_data.shape[0], self._max_per_bag, replace=False)
